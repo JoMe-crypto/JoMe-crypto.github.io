0885a4c1be5d3c724cd488ddb27940e461e56826
JoMe-crypto <johannes.melchert@student.uibk.ac.at>
2020-03-26 16:01:52 +0100

    save_Konsole

--- javascript/hello.js
+++ javascript/hello.js
@@ -1,1 +1,17 @@
 // alert('Hell world');
+
+// let message = 'Hello World';
+// alert(message);
+// message = 'Hallo Welt'
+// alert (message);
+
+const LINK_Color = '#ff0000';
+console log ('Link bitte inder Farbe', LINK_Color);
+
+let highscore = 520233;
+console.log(highscore /10);
+
+let firstname = 'John';
+let lastname ='Smith';
+console.log('Name: ', firstname, lastname);
+
