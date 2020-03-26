2c1514202cebf643bec990d1c687f70d1225c71e
JoMe-crypto <johannes.melchert@student.uibk.ac.at>
2020-03-26 16:52:25 +0100

    save_Schleifen

--- javascript/hello.js
+++ javascript/hello.js
@@ -54,13 +54,18 @@ console.log(b/(a-1));
 a++; 
 console.log(a);
 
-let my_age = prompt('Wie alt bist du?');
-console.log(`Du bischt ${my_age} Jahre alt.`);
-console.log( über 18? $my_age > 18);
+// // let my_age = prompt('Wie alt bist du?');
+// // console.log(`Du bischt ${my_age} Jahre alt.`);
+// // console.log(`über 18? ${my_age} > 18`);
 
-if(my_age > 18) {
-console.log('Glückwunsch über 18');
-} else {
-    console.log('leider unter 18');
+// // if(my_age > 18) {
+// // console.log('Glückwunsch über 18');
+// // } else {
+// //     console.log('leider unter 18');
+// }
+
+// Schleifen: for Schleife
+
+for (i=0; i<10; i++){
+    console.log(`Schleife ${i}`);
 }
-
