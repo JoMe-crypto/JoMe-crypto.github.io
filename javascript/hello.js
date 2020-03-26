9321c87ab1ab49b8585aca6d32bb6fa15e09a2fb
JoMe-crypto <johannes.melchert@student.uibk.ac.at>
2020-03-26 16:09:36 +0100

    save_Highscore

--- javascript/hello.js
+++ javascript/hello.js
@@ -5,8 +5,8 @@
 // message = 'Hallo Welt'
 // alert (message);
 
-const LINK_Color = '#ff0000';
-console log ('Link bitte inder Farbe', LINK_Color);
+const LINK_COLOR = '#ff0000';
+console.log ('Link bitte inder Farbe', LINK_COLOR);
 
 let highscore = 520233;
 console.log(highscore /10);
@@ -14,4 +14,12 @@ console.log(highscore /10);
 let firstname = 'John';
 let lastname ='Smith';
 console.log('Name: ', firstname, lastname);
+
+let fulname = 'Jeffrey "The Dude" Lebowski';
+console.log(fulname);
+
+let template = `Dein Highscore sind ${highscore} Punke`;
+console.log(template);
+
+
 
