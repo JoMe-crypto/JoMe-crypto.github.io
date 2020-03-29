5edf2470071e3e4af2ec3ccd03a2f87762db0a4c
JoMe-crypto <johannes.melchert@student.uibk.ac.at>
2020-03-29 17:12:04 +0200

    save_änderung width 100%

---
+++ nz/main.js
@@ -0,0 +1,5 @@
+var mymap =('t_map').setView([-38.7942, 175.7504], 11);
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    maxZoom: 17,
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+})
