23e75e1d434ad5b5aff5e7dddc270b21849061f2
JoMe-crypto <johannes.melchert@student.uibk.ac.at>
2020-04-01 14:47:41 +0200

    2 main.js - ReferenceError: L is not defined

--- nz/main.js
+++ nz/main.js
@@ -1,5 +1,7 @@
-var mymap =('t_map').setView([-38.7942, 175.7504], 11);
+let map = document.querySelector("#map")
+var mymap = L.map('map').setView([-38.796111, 175.905], 13);
+
 L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     maxZoom: 17,
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-})
+}).addTo(mymap);
