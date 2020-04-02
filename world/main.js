379e895adec065434d0967528e3dbb424be7271a
JoMe-crypto <johannes.melchert@student.uibk.ac.at>
2020-04-02 18:04:03 +0200

    komme nicht mehr mit

--- world/main.js
+++ world/main.js
@@ -1,36 +1,39 @@
 let startLayer = L.tileLayer.provider("OpenTopoMap");
-let map  = L.map("map", {
-    center:[0,0],
+let map = L.map("map", {
+    center: [0, 0],
     zoom: 2,
     layers: [
-    L.tileLayer.provider("OpenTopoMap")
+        L.tileLayer.provider("OpenTopoMap")
     ]
-}); 
+});
 
 let circleGroup = L.featureGroup().addTo(map);
 
 L.control.layers({
     "OpenTopoMap": L.tileLayer.provider("OpenTopoMap"),
-    "OpenStreetMap.Mapnik" : L.tileLayer.provider("OpenStreetMap.Mapnik"),
-    "Esri.OceanBasemap" : L.tileLayer.provider("Esri.OceanBasemap"),
-    "Esri.NatGeoWorldMap" : L.tileLayer.provider("Esri.NatGeoWorldMap"),
-    "HikeBike.HikeBike" :L.tileLayer.provider("HikeBike.HikeBike"),
-    "OpenRailwayMap" : L.tileLayer.provider("OpenRailwayMap"),
-},{
-"Thematische Darstellung" : circleGroup
+    "OpenStreetMap.Mapnik": L.tileLayer.provider("OpenStreetMap.Mapnik"),
+    "Esri.OceanBasemap": L.tileLayer.provider("Esri.OceanBasemap"),
+    "Esri.NatGeoWorldMap": L.tileLayer.provider("Esri.NatGeoWorldMap"),
+    "HikeBike.HikeBike": L.tileLayer.provider("HikeBike.HikeBike"),
+    "OpenRailwayMap": L.tileLayer.provider("OpenRailwayMap"),
+}, {
+    "Thematische Darstellung": circleGroup
 }).addTo(map);
 
-L.marker ([0,0]).addTo(map);
+L.marker([0, 0]).addTo(map);
+
+let drawCircle = function (data)
+
 //console.log(CONFIRMD);
 
 for (let i = 1; i < CONFIRMD.length; i++) {
     let row = CONFIRMD[i];
 
     //console.log(row [2], row [3]);
+// let rag =
+    let val = row[row.length - 1];
 
-    let val = row [row.length-1];
-
-   //let mrk = L.marker ([row [2], row [3]]).addTo(map);
+    //let mrk = L.marker ([row [2], row [3]]).addTo(map);
     //mrk.bindPopup(`${row[0]} ${row[1]}: ${val}`);
 
     //let circle = L.circleMarker ([row [2],row [3]]).addTo(map)
@@ -39,10 +42,12 @@ for (let i = 1; i < CONFIRMD.length; i++) {
     //r = WURZEL (A/PI)
 
     let s = 0.5;
-    let rad = Math.sqrt(val/Math.PI);
-    let circle = L.circleMarker ([lat,lng]),{
-        radius: r
-    }.addTo(map);
-    circle.bindPopup (`${reg}: ${val}`);
+    let rad = Math.sqrt(val / Math.PI);
+    let circle = L.circleMarker([lat, lng]),
+        {
+            radius: r
+        }.addTo(map);
+    circle.bindPopup(`${reg}: ${val}`);
 }
 
+drawCircle(CONFIRMD),
