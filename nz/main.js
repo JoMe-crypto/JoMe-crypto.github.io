a607662d11b47507601057c27f3c07e7a709adeb
JoMe-crypto <johannes.melchert@student.uibk.ac.at>
2020-04-02 16:16:38 +0200

    weltkarte grundgerüst

--- nz/main.js
+++ nz/main.js
@@ -1,7 +1,8 @@
 let map = document.querySelector("#map");
 var mymap = L.map('map').setView([-38.796111, 175.905], 9);
+let map = L.tileLayer.provider('OpenTopoMap'); 
 
-L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+/* L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     maxZoom: 17,
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 }).addTo(mymap);
@@ -13,7 +14,9 @@ var marker = L.marker([-38.68691, 176.06588]).addTo(mymap);
 //fillOpacity: 0.5,
 //radius: 500
 //}).addTo(mymap);
+*/
 
 marker.bindPopup("<b>Taupo </b><br>Marina").openPopup();
 //circle.bindPopup("I am a circle.");
 //polygon.bindPopup("I am a polygon.");
+
