77c19c6e3fececb8e11d5a03a9297c744088b0ce
JoMe-crypto <johannes.melchert@student.uibk.ac.at>
2020-04-02 18:11:04 +0200

    fehler in main.js gefunden

--- world/main.js
+++ world/main.js
@@ -22,32 +22,28 @@ L.control.layers({
 
 L.marker([0, 0]).addTo(map);
 
-let drawCircle = function (data)
+let drawCircles = function (data) {
+    //console.log(data);
+    for (let i = 1; i < data.length; i++) {
+        let row = data[i];
+        //console.log(row[2],row[3]);
+        let reg = `${row[0]} ${row[1]}`;
+        let lat = row[2];
+        let lng = row[3];
+        let val = row[row.length - 1];
+        //let mrk = L.marker([lat,lng]).addTo(map);
+        //mrk.bindPopup(`${reg}: ${val}`);
 
-//console.log(CONFIRMD);
-
-for (let i = 1; i < CONFIRMD.length; i++) {
-    let row = CONFIRMD[i];
-
-    //console.log(row [2], row [3]);
-// let rag =
-    let val = row[row.length - 1];
-
-    //let mrk = L.marker ([row [2], row [3]]).addTo(map);
-    //mrk.bindPopup(`${row[0]} ${row[1]}: ${val}`);
-
-    //let circle = L.circleMarker ([row [2],row [3]]).addTo(map)
-    //A = r²*PI
-    //r² = A/PI
-    //r = WURZEL (A/PI)
-
-    let s = 0.5;
-    let rad = Math.sqrt(val / Math.PI);
-    let circle = L.circleMarker([lat, lng]),
-        {
+        //A = r²*PI
+        //r² = A/PI
+        //r = WURZEL(A/PI)
+        let s = 0.5;
+        let r = Math.sqrt(val * s / Math.PI);
+        let circle = L.circleMarker([lat, lng], {
             radius: r
-        }.addTo(map);
-    circle.bindPopup(`${reg}: ${val}`);
-}
+        }).addTo(circleGroup);
+        circle.bindPopup(`${reg}: ${val}`);
+    }
+};
 
-drawCircle(CONFIRMD),
+drawCircles(CONFIRMED);
