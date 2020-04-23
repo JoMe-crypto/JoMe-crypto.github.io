5e127e14c41d8143eef2aa32a39d5209dde0f4a8
JoMe-crypto <johannes.melchert@student.uibk.ac.at>
2020-04-23 16:08:15 +0200

    geomertry

--- template_aws-tirol/main.js
+++ template_aws-tirol/main.js
@@ -10,7 +10,8 @@ let map = L.map("map", {
 
 // let awsLayer = L.featureGroup().addTo(map); könnte man jetzt löschen!!!
 let overlay = {
-    stations:L.featureGroup()
+    stations:L.featureGroup(), 
+    temperature: L.featureGroup()
 }
 
 L.control.layers({
@@ -26,7 +27,8 @@ L.control.layers({
         L.tileLayer.provider("BasemapAT.overlay")
     ])
 }, {
-    "Wetterstationen Tirol": overlay.stations
+    "Wetterstationen Tirol": overlay.stations, 
+    "Temperatur (°C)": overlay.temperature
 }).addTo(map);
 
 let awsUrl = "https://aws.openweb.cc/stations";
@@ -60,10 +62,20 @@ let aws = L.geoJson.ajax(awsUrl, {
     }
 }).addTo(overlay.stations);
 
-aws.on("data:loaded", function(){
-    console.log(aws.toGeoJSON());
+let drawTemperature = function(jsonData) {
+    console.log("aus der Funkation", jsonData);
+    L.geoJson(jsonData,{
+        pointToLayer: function(feature, latlng){
+            return L.marker(latlng, {
+            title: `${feature.properties.name} (${feature.geometry.coordinates[2]}m)`
+        })
+    }).addTo(overlay.temperature);
+}
 
+aws.on("data:loaded", function(){
+    //console.log(aws.toGeoJSON());
+drawTemperature(aws.toGeoJSON())
     map.fitBounds(overlay.stations.getBounds());
 
-    overlay.stations.addTo(map);
+    overlay.temperature.addTo(map);
 });
