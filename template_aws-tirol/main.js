b49a8bbb37c49fd3c156ab6cc3c2f55ed8915c7a
JoMe-crypto <johannes.melchert@student.uibk.ac.at>
2020-04-23 17:14:42 +0200

    warum auch immer jetzt funktinoiert es

--- template_aws-tirol/main.js
+++ template_aws-tirol/main.js
@@ -1,17 +1,15 @@
 let startLayer = L.tileLayer.provider("BasemapAT.grau");
 
 let map = L.map("map", {
-    //center: [47.3, 11.5],
-    //zoom: 8,  kann man löschen 
     layers: [
         startLayer
     ]
 });
 
-// let awsLayer = L.featureGroup().addTo(map); könnte man jetzt löschen!!!
 let overlay = {
-    stations:L.featureGroup(), 
-    temperature: L.featureGroup()
+    stations: L.featureGroup(),
+    temperature: L.featureGroup(),
+    wind: L.featureGroup()
 }
 
 L.control.layers({
@@ -27,8 +25,9 @@ L.control.layers({
         L.tileLayer.provider("BasemapAT.overlay")
     ])
 }, {
-    "Wetterstationen Tirol": overlay.stations, 
-    "Temperatur (°C)": overlay.temperature
+    "Wetterstationen Tirol": overlay.stations,
+    "Temperatur (°C)": overlay.temperature,
+    "Windgeschwindigkeit (km/h)": overlay.wind
 }).addTo(map);
 
 let awsUrl = "https://aws.openweb.cc/stations";
@@ -36,26 +35,21 @@ let awsUrl = "https://aws.openweb.cc/stations";
 let aws = L.geoJson.ajax(awsUrl, {
     filter: function (feature) {
         //console.log("Feature in filter: ", feature);
-        //return feature.geometry.coordinates[2] > 3000;
-        //if(feature.properties.LT < 5){
-        //  return true;
-        //} else {
-        //    return false;
-        //}
-        return feature.properties.LT !== null;
+        return feature.properties.LT;
     },
     pointToLayer: function (point, latlng) {
-        console.log("point: ", point);
+        // console.log("point: ", point);
         let marker = L.marker(latlng).bindPopup(`
         <h3>${point.properties.name} ${point.geometry.coordinates[2]} m</h3>
         <ul>
-        <li>Position: Lat: ${point.geometry.coordinates[1]}/Lng: ${point.geometry.coordinates[0]}</li>
         <li>Datum: ${point.properties.date}</li>
-        <li>Lufttemperatur: ${point.properties.LT} °C</li>
-        <li>Windgeschwindigkeit: ${point.properties.WG} m/s</li>
-        <li>Relative Luftfeuchtigkeit: ${point.properties.RH} % </li>
-        <li>Schneehöhe: ${point.properties.HS} cm </li>
-        <li> Grafik: <a href= https://lawine.tirol.gv.at/data/grafiken/1100/standard/tag/${point.properties.plot}.png> Link </a></li>
+        <li>Position (Lat,Lng): ${point.geometry.coordinates[1].toFixed(5)}, ${point.geometry.coordinates[0].toFixed(5)}</li>
+        <li>Lufttemperatur (°C): ${point.properties.LT}</li>
+        <li>Windgeschwindigkeit (m/s): ${point.properties.WG || "-"}</li>
+        <li>Relative Luftfeuchte (%): ${point.properties.RH || "-"}</li>
+        <li>Schneehöhe (cm): ${point.properties.HS || "-"}</li>
+        </ul>
+        <p><a target="plot" href="https://lawine.tirol.gv.at/data/grafiken/1100/standard/tag/${point.properties.plot}.png">Grafik der vorhandenen Messwerte anzeigen</a></p>
         </ul>
         `);
         return marker;
@@ -79,6 +73,11 @@ let drawTemperature = function(jsonData) {
         }
     }).addTo(overlay.temperature);
 };
+
+// 1. neues overlay definieren, zu L.control.layers hinzufügen und default anzeigen
+// 2. die Funktion drawWind als 1:1 Kopie von drawTemperature mit Anpassungen (in km/h)
+// 3. einen neuen Stil .label-wind im CSS von main.css
+// 4. die Funktion drawWind in data:loaded aufrufen
 
 let drawWind = function(jsonData) {
     //console.log("aus der Funktion", jsonData);
