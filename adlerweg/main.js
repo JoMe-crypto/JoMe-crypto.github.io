732dc97c87a51f5b63591086baee45494f810932
JoMe-crypto <johannes.melchert@student.uibk.ac.at>
2020-04-30 16:35:18 +0200

    adlerweg overlay

--- adlerweg/main.js
+++ adlerweg/main.js
@@ -8,6 +8,10 @@ let map = L.map("map", {
     ]
 });
 
+let overlay = {
+    adlerblicke: L.featureGroup()
+};
+
 L.control.layers({
     "BasemapAT.grau": L.tileLayer.provider("BasemapAT.grau"),
     "BasemapAT": L.tileLayer.provider("BasemapAT"),
@@ -20,7 +24,10 @@ L.control.layers({
         L.tileLayer.provider("BasemapAT.orthofoto"),
         L.tileLayer.provider("BasemapAT.overlay")
     ])
+}, {
+    "Adlerblicke": overlay.adlerblicke
 }).addTo(map);
+
 
 //console.log(ETAPPEN);
 //console.log(ADLERBLICKE);
@@ -30,3 +37,4 @@ for (const blick of ADLERBLICKE) {
     let mrk = L.marker([blick.lat,blick.lng]).addTo(map);
     mrk.bindPopup(`Standort ${blick.standort} (${blick.seehoehe}m)`);
 }
+overlay.aderlblicke.addTo(map);
