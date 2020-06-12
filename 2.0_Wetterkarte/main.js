b7b40e1119d7ce348025efc4d5e927ca564f7e7c
JoMe-crypto <johannes.melchert@student.uibk.ac.at>
2020-06-12 21:56:34 +0200

    erster Erfolg

--- 2.0_Wetterkarte/main.js
+++ 2.0_Wetterkarte/main.js
@@ -1,27 +1,48 @@
+
 function weatherInfo(id=2761369) {
 var key = '{e105b8b9538822cec7ff5f7d006f2e9a}';
 fetch('http://api.openweathermap.org/data/2.5/forecast?q=Vienna,AT&units=metric&APPID=e105b8b9538822cec7ff5f7d006f2e9a')  
     .then(function(resp) { return resp.json() }) 
     .then(function(data) {
-      showWeather(data);
+      showForecast(data);
     })
     .catch(function() {
     
     });
   }
+
+  window.onload = function() {
+    weatherInfo(2761369);
+  }
+
+  function showForecast (d) {
+    //var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+    //var date = d.sys.dt_txt; 
+      //document.getElementById('description').innerHTML = d.weather[0].description;
+      document.getElementById('temp').innerHTML = d.list[0].main.temp + '&deg;';
+      //document.getElementById('name').innerHTML = d.city.name;
+      //document.getElementById('pressure').innerHTML = d.main[0].pressure +'hPa';
+      //document.getElementById('wind').innerHTML = d.wind[0].speed + 'm/s';
+      //document.getElementById('humidity').innerHTML = d.main[0].humidity + '%'
+  
+  }
+
+  
+
  
-  function showWeather (d) {
+
+  /*function showWeather (d) {
     var celcius = Math.round(parseFloat(d.main.temp)-273.15);
-    var date = d.sys.dt_txt; 
-      document.getElementById('description').innerHTML = d.weather[0].description;
-      document.getElementById('temp').innerHTML = d.main[0].temp + '&deg;';
+      document.getElementById('description').innerHTML = d.weather[1].description;
+      document.getElementById('temp').innerHTML = d.main[1].temp + '&deg;';
       document.getElementById('name').innerHTML = d.city.name;
-      document.getElementById('pressure').innerHTML = d.main[0].pressure +'hPa';
-      document.getElementById('wind').innerHTML = d.wind[0].speed + 'm/s';
-      document.getElementById('humidity').innerHTML = d.main[0].humidity + '%'
+      document.getElementById('pressure').innerHTML = d.main[1].pressure +'hPa';
+      document.getElementById('wind').innerHTML = d.wind[1].speed + 'm/s';
+      document.getElementById('humidity').innerHTML = d.main[1].humidity + '%'
     
   }
 
   window.onload = function() {
     weatherInfo(2761369);
-  }
+  } 
+  */
